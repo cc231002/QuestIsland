5b907dca8f38f672de8fddbc39c309f38d88aaab
Máté Bráder <146343488+theanimatt@users.noreply.github.com>
2025-06-13 10:50:28 +0200

    Bg music and walk

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,17 +13,54 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_BEE = 3093209662U;
+        static const AkUniqueID PLAY_BIRDS = 3025897706U;
         static const AkUniqueID PLAY_BUTTONCLICK = 3548535810U;
+        static const AkUniqueID PLAY_COMPLETE = 2401712799U;
+        static const AkUniqueID PLAY_CORRECT = 3335480108U;
+        static const AkUniqueID PLAY_HEARTLOSE = 3271090145U;
+        static const AkUniqueID PLAY_HEARTUP = 3022337573U;
+        static const AkUniqueID PLAY_MAINMUSIC = 1346240278U;
         static const AkUniqueID PLAY_OVER = 2348692292U;
         static const AkUniqueID PLAY_STARTMUSIC = 1035306431U;
+        static const AkUniqueID PLAY_TIMER = 4169444829U;
+        static const AkUniqueID PLAY_TRIVIAMUSIC = 1573978988U;
+        static const AkUniqueID PLAY_WALK = 1589278981U;
+        static const AkUniqueID PLAY_WATERFALL = 467174588U;
+        static const AkUniqueID PLAY_WAVES = 374167564U;
         static const AkUniqueID PLAY_WIN = 2955987680U;
+        static const AkUniqueID PLAY_WIND = 1020223172U;
+        static const AkUniqueID PLAY_WRONG = 92581801U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SURFACE
+        {
+            static const AkUniqueID GROUP = 1834394558U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace SURFACE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TIMELEFT = 2486342905U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID MAINSOUNDS = 82810526U;
         static const AkUniqueID STARTING = 399468041U;
+        static const AkUniqueID TRIVIA = 2563809248U;
     } // namespace BANKS
 
     namespace BUSSES
